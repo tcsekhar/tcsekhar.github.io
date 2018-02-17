c6b14b8db8177d4643f9e85bb522d2542e5b9657
Chandra Sekhar <mail@sekhar.net>
2018-02-17 14:38:29 -0600

    All Files in project checked in
    All Files in project checked in

---
+++ js/Mining.js
@@ -0,0 +1,103 @@
+﻿    function fnGetBars(data)
+    {
+        $("#time").html(data.LastUpdateDate);
+        var pct = (data.CurrentTotalHashRate / data.MaxTotalHashRate) * 100;
+        $("#overallbar").css("width", pct + '%');
+        $("#score").html(Math.round(data.CurrentTotalHashRate) + "/" + Math.round(data.MaxTotalHashRate));
+        var itms = data.Miners;
+        var pct;
+        var color;
+        var output = '';
+        for (var j = 0; j < itms.length; j++) {
+            var html = '';
+            html += '<div id="' + itms[j].miner + '" class="Mstr" ht="' + chartHt + '" ip="' + itms[j].IPAddress + '" minerid="' + itms[j].MinerID + '" onClick="pickMiner(\'' + itms[j].miner + '\');">';
+            var gpus = itms[j].gpus;
+            var maxhash = itms[j].maxhash;
+            var currhash = itms[j].hash;
+            for (var i = 0; i < gpus.length; i++) {
+                pct = (1 - ((maxhash / gpus.length) - gpus[i].hash) / (maxhash / gpus.length)) * 100;
+                color = 'green';
+                if (pct <= 85 && pct >= 60) color = 'yellow';
+                if (pct < 60) color = 'red';
+                if (pct == 0) color = 'zero';
+                var bor = 'greenborder';
+                if (gpus[i].temp <= 75 && gpus[i].temp >= 60) bor = 'yellowborder';
+                if (gpus[i].temp > 75) bor = 'redborder';
+                var faran = gpus[i].temp * 9 / 5 + 32;
+                html += '<div title="Hashrate: ' + gpus[i].hash + '\nTemp: ' + gpus[i].temp + '°C / ' + faran + '°F\nFan: ' + gpus[i].fan + '%" tooltip=title="Hashrate: ' + gpus[i].hash + '\nTemp: ' + gpus[i].temp + '°C / ' + faran + '°F\nFan: ' + gpus[i].fan + '%" class="container"><div class="Child ' + color + ' ' + bor + '" ht=' + pct + '></div>' + Math.round(gpus[i].hash) + '</div>';
+            }
+            pct = (1 - (itms[j].maxhash - itms[j].hash) / itms[j].maxhash) * 100;
+            color = 'green';
+            if (pct < 85 && pct > 60) color = 'yellow';
+            if (pct < 60) color = 'red';
+            var alerts = '<div class="alerts"><div><a class="cnt">' + itms[j].Min30 + '</a><a class="duration">30 m</a></div><div><a class="cnt">' + itms[j].Hr2 + '</a><a class="duration">2 h</a></div><div><a class="cnt">' + itms[j].Hr6 + '</a><a class="duration">6 h</a></div><div><a class="cnt">' + itms[j].D1 + '</a><a class="duration">1 d</a></div><div><a class="cnt">' + itms[j].D2 + '</a><a class="duration">2 d</a></div></div>'
+            html += '<div class="Miner"><div class="minerbar ' + color + '" wd="' + pct + '"></div>' + Math.round(itms[j].hash) + '/' + Math.round(itms[j].maxhash) + '</div>' + alerts + '<input type="button" class="btnMiner" id="btn' + itms[j].miner + '" value="' + itms[j].miner + '" ></div>';
+
+            if ($("#" + itms[j].miner).length > 0)
+                $("#" + itms[j].miner).replaceWith(html);
+            else
+                $("#cnt").append(html);
+        }
+        $(".Mstr").each(function () { $(".container").css("height", $(this).attr("ht") + 'px'); });
+        $(".Child").each(function () { $(this).css("height", $(this).attr("ht") + '%'); });
+        $(".minerbar").each(function () { $(this).css("width", $(this).attr("wd") + '%'); });
+        $(".cnt").each(function () { if ($(this).html() != "0") $(this).css("background-color", "red"); });
+        pickMiner('');
+    }
+
+
+    function pickMiner(Miner)
+    {
+        if (Miner =='')
+            Miner = $("#selectedMiner").val();
+        else
+            $("#selectedMiner").val(Miner);
+        $(".Mstr").attr("class", "Mstr");
+        $(".btnMiner").attr("class", "btnMiner");
+        var id = '#btn' + Miner;
+        $(id).attr("class", "btnMiner chart");
+        if (Miner != '') {
+            var id = '#' + Miner;
+            $(id).attr("class", "Mstr Sel");
+            getChart('GPU', Miner);
+        }
+    }
+
+    function getAllMinersChart() {
+        $.get('GetData.aspx?charttype=ALL', function (data) {
+            gpus = JSON.parse(data);
+            var ctx3 = document.getElementById("gpus").getContext("2d");
+            window.myLine = new Chart(ctx3, gpus);
+            $("#btnShowSSH").attr("class", "");
+            $("#btnShowSSH").attr("class", "hidden");
+            $("#btnShowSSH").attr("Miner", '');
+            $("#selectedMiner").val('');
+            $(".Mstr").attr("class", "Mstr");
+            $(".btnMiner").attr("class", "btnMiner");
+        });
+    }    
+    
+    function getChart(type, Miner) {
+        $("#chartLoc").html('<canvas id="gpus"></canvas>');
+        var id = '#' + Miner;
+        var MinerID = $(id).attr("minerid");
+        $.get('GetData.aspx?charttype=' + type + '&MinerID=' + MinerID, function (data) {
+            gpus = JSON.parse(data);
+            var ctx3 = document.getElementById("gpus").getContext("2d");
+            window.myLine = new Chart(ctx3, gpus);
+            if (Miner != '') {
+                $("#btnShowSSH").attr("Miner",Miner);
+                $("#btnShowSSH").attr("class", "");
+                $("#btnShowSSH").val("Login " + Miner + " [SSH]");
+            }
+            else
+                $("#btnShowSSH").attr("class", "hidden");
+        });
+    }
+
+    function showSSH()
+    {
+        var Miner = $("#selectedMiner").val();
+        var id = '#' + Miner;
+        $("#iframe").html('<iframe src="http://' + $(id).attr("ip") + '/" frameborder="0" scrolling="no" id="sshframe" height="100%" width="100%">')
+    }
